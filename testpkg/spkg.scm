31f52d36dd59f84d580a04169ff8778a127b42ee
playX18 <adel.prokurov281@outlook.com>
2025-12-16 14:48:33 +0700

    remove unneded testpkg

--- testpkg/spkg.scm
+++
@@ -1,7 +0,0 @@
-(package
-  (name (testpkg))
-  (version "0.1.0")
-  (description "A new Scheme package.")
-  (rnrs r7rs))
-
-(dependencies)
--- testpkg/src/testpkg.sld
+++
@@ -1,8 +0,0 @@
-(define-library testpkg
-  (export add1)
-  (import (scheme base))
-
-  (begin
-    (define (add1 x)
-      (+ x 1))
-  ))
